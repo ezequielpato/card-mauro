c8536efe3c1c49ca996fcf06b965320f27a7d9e2
ezequielpato <ezequiel.salomon@gmail.com>
2023-02-06 22:58:48 -0300

    card basica

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Card  from './components/card/Card.jsx'
+
+import './components/card/card.css'
+
+function App() {
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './card.css'
+
+const Card = () => {
+    const [state, setState] = useState(0)
+
+
+    return (
+        <section>
+            <div className='container'>
+                <div className='card-container'>
+                    {/* <h4>card</h4> */}
+                    <div className='card-img'>
+                        {/* <p>img</p> */}
+                        <img src='https://www.espaciovino.com.ar/media/default/0001/61/thumb_60729_default_medium.jpeg' width="250px" height="auto"></img>
+                    </div>
+                    <div className='card-body'>
+                        <h3>Card Title</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, voluptatum.</p>
+                    </div>
+                    <div className='card-footer'>
+                        <div className='card-button' onClick={()=> setState(state+1)}><p>+</p></div>
+                            <p>{state}</p>
+                        <div className='card-button' onClick={()=> {state > 0 && setState(state-1)}}><p>-</p></div>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Card
